d48dbe52af7b8d006ba5f976f6a2672cdc4992fc
mnqoliveira <mnq.oliveira@gmail.com>
2018-10-22 10:05:46 -0300

    inclui segundo exercicio

--- aula01_exercicios.R
+++ aula01_exercicios.R
@@ -1,10 +1,19 @@
 
 
-# Exercicio ---------------------------------------------------------------
 
+# Exercicio 1 -------------------------------------------------------------
 # Criar um conjunto de dados correspondentes aos pontos de um campeonato de 
 # futebol e atribuir as 
 # pontuacoes de pelo menos cinco clubes. Indique nas linhas os nomes dos 
 # clubes e nas colunas o total de pontos, o numero de vitorias e o numero de
 # empates. O total de pontos deve ser calculado automaticamente em funcao
 # do numero de empates (1 ponto) e de vitorias (3 pontos)
+
+
+
+
+# Exercicio 2 -------------------------------------------------------------
+# Criar um conjunto de dados com duas colunas e dez linhas. Na primeira, 
+# o primeiro valor corresponde a sua idade e o ultimo, a idade do seu pai. Na 
+# segunda, voce deve indicar se o numero eh divisivel por 3.
+
