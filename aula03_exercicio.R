f479f34525cd72dea253a7c9aae0809133ce3a2d
mnqoliveira <mnq.oliveira@gmail.com>
2018-10-31 17:47:15 -0300

    inclusao do exercicio

---
+++ aula03_exercicio.R
@@ -0,0 +1,18 @@
+# Utilizando o codigo visto em aula para leitura de varios arquivos, realizar
+# o mesmo procedimento mas salvando apenas os dados referentes ao que ocorre
+# antes da ruptura da batata. Para estes dados, e para cada amostra, 
+# calcular a diferenca entre o maior e o menor valor de forca aplicado 
+# e dividir pelo tempo decorrido entre esta aplicacao. Salve todos os valores
+# em apenas uma variavel, em que uma coluna corresponde ao resultado
+# dessa divisao e em que a outra corresponde ao nome da amostra.
+# 
+# Dicas:
+# Em cada iteração do loop, adicionalmente:
+# 1. Identifique o ponto em que o valor da forca eh negativo e 
+# utilize este ponto para determinar quando ocorre a ruptura
+# 2. Inclua uma coluna a cada arquivo lido com um valor associado ao numero
+# da linha. Pode ser mais facil determinar ate qual linha voce deve salvar.
+# 3. Crie a variavel nova da mesma forma como foi criada a variavel 
+# conjunto_total
+
+
